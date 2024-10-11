c78a5ef96d43c18967b12ed99df3c5f00ee6ca1c
JyotiV <vermajyoti2626.jv@gmail.com>
2024-10-11 17:04:45 +0530

    Commit files from 9th Sep

---
+++ src/Sept/ex_09092024_Comments/Lab009.java
@@ -0,0 +1,21 @@
+package Sept.ex_09092024_Comments;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        // This is single line comment
+        // This will not be executed
+
+        /*
+         * Multiple Line Comments
+         *
+         * */
+
+
+        /**
+         * Java Documentation Multiple Line Comments
+         * <p>
+         *
+         *
+         * **/
+    }
+}
---
+++ src/Sept/ex_09092024_Comments/Lab010.java
@@ -0,0 +1,13 @@
+package Sept.ex_09092024_Comments;
+
+public class Lab010 {
+    public static void main(String[] args) {
+        //Introduction to data types
+        // Keywords
+        // Reserved Words
+        // Simple English -> Pre Defined meaning in java programs.
+        // they are in blue in intelliJ
+        // package, public,class, static, void
+        // Lower case
+    }
+}
