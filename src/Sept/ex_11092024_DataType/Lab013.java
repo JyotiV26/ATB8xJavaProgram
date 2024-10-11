338349647100df40edd795fd9ac6399069a3e33e
JyotiV <vermajyoti2626.jv@gmail.com>
2024-10-11 16:55:09 +0530

    Commit files from 11th Sep

---
+++ src/Sept/ex_11092024_DataType/Lab013.java
@@ -0,0 +1,19 @@
+package Sept.ex_11092024_DataType;
+
+public class Lab013 {
+    public static void main(String[] args) {
+        // variable_name (Identifier) - Rules we have to follow
+//        boolean 123 = true;
+        boolean $ = true;
+        boolean _= true; //For jdk 21, preview should be open to use identifier '_' (Command+;and pick "Preview" option from the Language Level dropdown)
+        boolean pramod_is_very_good_person = true;
+//        boolean 123pramod = true;
+        boolean pramod123 = true;
+        boolean _123 = true;
+        System.out.println(_123);
+        boolean PRAMOD = true;
+        boolean pramod$dutta = true;
+        boolean pramod_dutta = true;
+    }
+
+}
---
+++ src/Sept/ex_11092024_DataType/Lab018.java
@@ -0,0 +1,7 @@
+package Sept.ex_11092024_DataType;
+
+public class Lab018 {
+    public static void main(String[] args) {
+        //no code
+    }
+}
