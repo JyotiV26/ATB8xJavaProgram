b1ed14dbf8e0330f6267f22899445651a492248a
JyotiV <vermajyoti2626.jv@gmail.com>
2024-10-11 17:14:38 +0530

    Last commit for 6th Sep

---
+++ src/Sept/ex_06092024/Lab005.java
@@ -0,0 +1,16 @@
+package Sept.ex_06092024;
+
+public class Lab005 {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        main("pramod");//method overloading
+    }
+    public static void main(String args){
+        System.out.println("Hello World!");
+    }
+
+    public void main(int args){
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/Sept/ex_06092024/Lab007.java
@@ -0,0 +1,19 @@
+package Sept.ex_06092024;
+//Intro to commenting in Java
+
+public class Lab007 {
+    public static void main(String[] args) {
+        // This code will not be executed.
+        // This is used to show the user or dev instructions
+        // You can write anything here
+
+        /* Multi line comment
+         *
+         * Write Anything here
+         *
+         * */
+
+        System.out.println("Hi");
+
+    }
+}
