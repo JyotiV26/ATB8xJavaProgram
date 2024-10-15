ee6ed8d15d39dfda8274afb611050780424a316f
JyotiV <vermajyoti2626.jv@gmail.com>
2024-10-15 12:57:57 +0530

    Files from 18th Sept

---
+++ src/Sept/ex_18092024/Lab050.java
@@ -0,0 +1,23 @@
+package Sept.ex_18092024;
+
+public class Lab050
+{
+    public static void main(String[] args) {
+        // Type Casting
+        // Widening - Implicit, Explicit - lossless
+        // Narrowing - Implicit, Explicit(with data type), loss
+
+        // Widening Example
+        byte b = 10;
+        int a = b; // Valid - Implicit Casting - JVM
+        int a1 = (int)b; // Valid - Explicit Casting - JVM
+        System.out.println(a1);
+
+        // Narrowing Example
+        int val = 200;
+        //byte b1 = val; // Invalid Implicit Casting - JVM, not allowed
+        byte b2 = (byte)val; // Invalid Explicit Casting - Loss of data.
+        System.out.println(b2);
+
+}
+}
